f0983ca28e7199ae5376ce1a5e1bde4a0016cc73
tinbolw <tinbolw@gmail.com>
2020-06-02 18:30:15 -0700

    added the four basic operations

--- main.py
+++ main.py
@@ -23,15 +23,35 @@ def askFirstNumber():
 
 def operate():
     global Answer
-    if operation == str("multiply"):
+    if operation == str("multiply") or operation == str("Multiply"):
         Answer = int(FirstNumber * SecondNumber)
         print("The answer is " + str(Answer))
+    elif operation == str("add") or operation == str("Add"):
+        Answer = int(FirstNumber + SecondNumber)
+        print("The answer is " + str(Answer))
+    elif operation == str("subtract") or operation == str("Subtract"):
+        Answer = int(FirstNumber - SecondNumber)
+        print("The answer is " + str(Answer))
+    elif operation == str("divide") or operation == str("Divide"):
+        Answer = int(FirstNumber / SecondNumber)
+        print("The answer is " + str(Answer))
+    elif operation == str("power") or operation == str("Power")
+        Answer = int(FirstNumber ^ SecondNumber)
+        print("The answer is " + str(Answer))
+
+
 
 
 print("Operations: *, /, -, +, ^, √, or random number")
 askOperation()
-if operation.isalpha():
+if operation.isalpha() and operation == str("Multiply") or operation == str("multiply"):
+    askFirstNumber()
+elif operation.isalpha() and operation == str("Divide") or operation == str("divide"):
+    askFirstNumber()
+elif operation.isalpha() and operation == str("Add") or operation == str("add"):
+    askFirstNumber()
+elif operation.isalpha() and operation == str("Subtract") or operation == str("subtract"):
     askFirstNumber()
 else:
-    print("invalid operation (type it as a word)")
+    print("invalid operation (type it as a word)(power, sqrt)")
     askOperation()
