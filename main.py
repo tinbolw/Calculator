1d402d34fb0c07e5c1baaadf36e32c4c74f1d641
tinbolw <tinbolw@gmail.com>
2020-06-09 14:15:06 -0700

    made it so you can restart, and operation bug where if you chose the wrong operation and tried again it would close is now fixed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-
+import re
 FirstNumber = 0
 SecondNumber = 0
 operation = "??"
@@ -8,7 +8,20 @@ SqrtAns = 0
 SqrtNumber = 0
 RandomNumber = 0
 maxnum = 0
-
+useAgainput = None
+def useAgain():
+    global useAgainput
+    global operation
+    useAgainput = input("Do you want to try again? (Y/N)\n")
+    if useAgainput == "Y":
+        operation = 3.14159
+        askOperation()
+    elif useAgainput == "N":
+        print("Okay. Goodbye!")
+        quit()
+    else:
+        print("Invalid Selection")
+        useAgain()
 
 def getRandomNumber():
     global RandomNumber
@@ -16,6 +29,7 @@ def getRandomNumber():
     maxnum = int(input("What is the limit to the random number? "))
     RandomNumber = random.randint(1, maxnum)
     print("The random number is: " + str(RandomNumber))
+    useAgain()
 
 
 def askSqrtNumber():
@@ -24,11 +38,39 @@ def askSqrtNumber():
     SqrtNumber = int(input("What is the number? "))
     SqrtAns = SqrtNumber ** 0.5
     print("This is the answer: " + str((SqrtAns)))
+    useAgain()
 
 
 def askOperation():
     global operation
-    operation = input("Operation: ")
+    if operation == 3.14159:
+        operation = input("Operation: ")
+        if operation.isalpha() and operation == str("Multiply") or operation == str("multiply"):
+            askFirstNumber()
+        elif operation.isalpha() and operation == str("Divide") or operation == str("divide"):
+            askFirstNumber()
+        elif operation.isalpha() and operation == str("Add") or operation == str("add"):
+            askFirstNumber()
+        elif operation.isalpha() and operation == str("Subtract") or operation == str("subtract"):
+            askFirstNumber()
+        elif operation.isalpha() and operation == str("square root") or operation == str(
+                "Square root") or operation == str(
+                "square Root") or operation == str("Square Root") or operation == str("sqrt") or operation == str(
+            "Sqrt") or operation == str("SQrt") or operation == str("SQRT") or operation == str(
+            "sQRT") or operation == str(
+            "sqRT") or operation == str("sqrT") or operation == str("SqRt") or operation == str("sQrT"):
+            askSqrtNumber()
+        elif operation.isalpha() and operation == str("random"):
+            getRandomNumber()
+        else:
+            operation = float("3.14159")
+            print("invalid operation)")
+            print("Operations:\nAdd\nSubtract\nDivide\nMultiply\nPower\nRandom\n")
+            askOperation()
+    else:
+        operation = input("Operation: ")
+
+
 
 
 def askSecondNumber():
@@ -48,21 +90,26 @@ def operate():
     if operation == str("multiply") or operation == str("Multiply"):
         Answer = int(FirstNumber * SecondNumber)
         print("The answer is " + str(Answer))
+        useAgain()
     elif operation == str("add") or operation == str("Add"):
         Answer = int(FirstNumber + SecondNumber)
         print("The answer is " + str(Answer))
+        useAgain()
     elif operation == str("subtract") or operation == str("Subtract"):
         Answer = int(FirstNumber - SecondNumber)
         print("The answer is " + str(Answer))
+        useAgain()
     elif operation == str("divide") or operation == str("Divide"):
         Answer = int(FirstNumber / SecondNumber)
         print("The answer is " + str(Answer))
+        useAgain()
     elif operation == str("power") or operation == str("Power"):
         Answer = int(FirstNumber ** SecondNumber)
         print("The answer is " + str(Answer))
+        useAgain()
 
 
-print("Operations: *, /, -, +, ^, √, or random number")
+print("Operations:\nAdd / Subtract\nMultiply / Divide\nSqrt /Power \nRandom\n")
 askOperation()
 if operation.isalpha() and operation == str("Multiply") or operation == str("multiply"):
     askFirstNumber()
@@ -72,14 +119,12 @@ elif operation.isalpha() and operation == str("Add") or operation == str("add"):
     askFirstNumber()
 elif operation.isalpha() and operation == str("Subtract") or operation == str("subtract"):
     askFirstNumber()
-elif operation.isalpha() and operation == str("square root") or operation == str("Square root") or operation == str(
-        "square Root") or operation == str("Square Root") or operation == str("sqrt") or operation == str(
-        "Sqrt") or operation == str("SQrt") or operation == str("SQRT") or operation == str("sQRT") or operation == str(
-        "sqRT") or operation == str("sqrT") or operation == str("SqRt") or operation == str("sQrT"):
+elif operation.isalpha() and operation == str("SQRT"):
     askSqrtNumber()
 elif operation.isalpha() and operation == str("random"):
     getRandomNumber()
 else:
-    print("invalid operation)")
-    print("Operations:\nAdd\nSubtract\nDivide\nMultiply\nPower\nRandom\n")
+    operation = float("3.14159")
+    print("invalid operation (its case sensitive)")
+    print("Operations:\nAdd\nSubtract\nDivide\nMultiply\nSQRT\nPower\nRandom\n")
     askOperation()
